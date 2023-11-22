e1ae266acd9d0695f504a46c4e81a4a47d2b7677
juliocaa <134210748+juliocaa@users.noreply.github.com>
2023-11-22 09:59:50 +0100

    Update aero_resistence_calculator.py

--- aero_resistence_calculator.py
+++ aero_resistence_calculator.py
@@ -1,36 +1,25 @@
 import streamlit as st
 
-# Título de la aplicación
-st.title('Calculadora de Resistencia Aerodinámica')
+# Define los valores por defecto para el Tesla Model 3
+DEFAULT_COEFICIENTE_DRAG = 0.23
+DEFAULT_AREA_FRONTAL = 2.2
+DENSIDAD_AIRE = 1.225  # en kg/m^3
 
-# Inputs del usuario
-st.sidebar.header('Parámetros de entrada')
-velocidad_kmh = st.sidebar.number_input('Introduce la velocidad en km/h', min_value=0.0, value=120.0, step=1.0)
-coeficiente_aerodinamico = st.sidebar.number_input('Coeficiente aerodinámico', min_value=0.0, value=0.23, step=0.01)
-area_frontal = st.sidebar.number_input('Área frontal en m²', min_value=0.0, value=2.2, step=0.1)
+# Título de la calculadora
+st.title('Calculadora de consumo de energía debido a la resistencia aerodinámica')
 
-# Conversión de la velocidad de km/h a m/s
+# Entradas del usuario
+velocidad_kmh = st.number_input('Introduzca la velocidad en km/h:', min_value=0.0, value=120.0)
+coeficiente_drag = st.number_input('Coeficiente aerodinámico del vehículo:', min_value=0.0, value=DEFAULT_COEFICIENTE_DRAG)
+area_frontal = st.number_input('Área frontal del vehículo (en m²):', min_value=0.0, value=DEFAULT_AREA_FRONTAL)
+
+# Conversión de velocidad a m/s
 velocidad_ms = velocidad_kmh / 3.6
 
-# Cálculo de la resistencia aerodinámica
-def calcular_resistencia_aerodinamica(velocidad, coef_drag, area_frontal):
-    densidad_aire = 1.225  # en kg/m³
-    resistencia_aerodinamica = 0.5 * densidad_aire * area_frontal * coef_drag * velocidad ** 2
-    return resistencia_aerodinamica
+# Cálculo de la potencia necesaria en vatios
+potencia_w = 0.5 * DENSIDAD_AIRE * area_frontal * coeficiente_drag * velocidad_ms**3
+potencia_kw = potencia_w / 1000  # Conversión de vatios a kilovatios
 
-resistencia_aerodinamica = calcular_resistencia_aerodinamica(velocidad_ms, coeficiente_aerodinamico, area_frontal)
+# Muestra la potencia necesaria para superar la resistencia del aire
+st.write(f'La potencia necesaria para superar la resistencia aerodinámica a {velocidad_kmh} km/h es: {potencia_kw:.2f} kW')
 
-# Mostrar el resultado
-st.write(f"La resistencia aerodinámica a {velocidad_kmh} km/h es: {resistencia_aerodinamica:.2f} N")
-
-# Mostrar información adicional
-st.markdown("""
-La fórmula utilizada para calcular la resistencia aerodinámica es:
-\[ R = \frac{1}{2} \cdot \rho \cdot A \cdot C_d \cdot v^2 \]
-donde:
-- \( R \) es la resistencia aerodinámica en Newtons (N).
-- \( \rho \) es la densidad del aire (1.225 kg/m³ a nivel del mar).
-- \( A \) es el área frontal del vehículo en metros cuadrados (m²).
-- \( C_d \) es el coeficiente de resistencia aerodinámica.
-- \( v \) es la velocidad en metros por segundo (m/s).
-""")
