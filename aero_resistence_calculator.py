c3c409670c8cb7b1b2005208a2e79c7be522da8f
juliocaa <134210748+juliocaa@users.noreply.github.com>
2023-11-26 17:59:45 +0100

    Update aero_resistence_calculator.py

--- aero_resistence_calculator.py
+++ aero_resistence_calculator.py
@@ -10,62 +10,60 @@ st.image(image, caption="AUTOINGENIUM", use_column_width=True)
 
 st.markdown("[VIDEO TUTORIAL](https://www.youtube.com/watch?v=gLxunTcgm1I)")
 
-import streamlit as st
+# Function to calculate the aerodynamic consumption
+def aerodynamic_consumption(cx, frontal_area, speed):
+    # Assuming air density at sea level and 15°C is 1.225 kg/m³
+    rho = 1.225
+    # Aerodynamic power consumption in kW
+    power = 0.5 * rho * speed**2 * frontal_area * cx
+    # Convert power to kWh/100km
+    consumption = power * speed / 1000
+    return consumption
 
-def calculate_aerodynamic_drag(cx, frontal_area, speed):
-    # Constante para la densidad del aire a nivel del mar y 15°C
-    air_density = 1.225  # en kg/m^3
-    # Calcula la fuerza de arrastre aerodinámico
-    drag_force = 0.5 * air_density * (speed ** 2) * frontal_area * cx
-    # Suponiendo una potencia constante para superar esta fuerza a la velocidad dada
-    # Potencia = Fuerza * Velocidad
-    # Energía = Potencia * Tiempo
-    # Energía en kWh para 100 km
-    energy_consumption = drag_force * speed / 3600  # Convertir W a kW y segundos a horas
-    return energy_consumption
-
-def calculate_additional_consumption(temperature, include_climate_control):
-    optimal_temperature = 20  # Temperatura óptima en grados Celsius
-    base_consumption = 5.5  # Consumo base en kWh cada 100 km
-
-    # Ajustar el consumo base en función de la pérdida de eficiencia
-    if temperature < optimal_temperature:
-        efficiency_loss = (optimal_temperature - temperature) / 20 * 0.3
+# Function to calculate the battery consumption based on temperature
+def battery_consumption(temperature, use_climate_control):
+    # Base consumption at optimal temperature (20°C)
+    base_consumption = 5.5
+    # Adjust consumption based on temperature
+    if temperature != 20:
+        efficiency_change = 0.3 * (1 - abs(temperature - 20) / 20)
+        temperature_consumption = base_consumption / (1 - efficiency_change)
     else:
-        efficiency_loss = (temperature - optimal_temperature) / 20 * 0.3
+        temperature_consumption = base_consumption
+    
+    # Adjust for climate control
+    climate_control_consumption = 0
+    if use_climate_control:
+        if temperature < 20:
+            climate_control_consumption = 0.7 * (20 - temperature) / 14
+        elif temperature > 20:
+            climate_control_consumption = 0.7 * (temperature - 20) / 20
+    
+    total_consumption = temperature_consumption + climate_control_consumption
+    return total_consumption
 
-    adjusted_consumption = base_consumption * (1 + efficiency_loss)
+# Streamlit app
+def app():
+    st.title('Electric Car Consumption Calculator')
 
-    # Añadir un consumo fijo si se incluye el control climático
-    climate_control_consumption = 2 if include_climate_control else 0
+    # Input fields with default values
+    cx = st.number_input('Coefficient of drag (Cx)', min_value=0.0, value=0.22)
+    frontal_area = st.number_input('Frontal Area (m²)', min_value=0.0, value=2.22)
+    temperature = st.number_input('Outside Temperature (°C)', value=20)
+    speed = st.number_input('Speed (km/h)', min_value=0.0, value=100.0)
+    use_climate_control = st.checkbox('Climate Control', value=False)
 
-    total_additional_consumption = adjusted_consumption + climate_control_consumption
+    # Calculate button
+    if st.button('Calculate'):
+        aero_consumption = aerodynamic_consumption(cx, frontal_area, speed)
+        battery_consumption = battery_consumption(temperature, use_climate_control)
+        total_consumption = aero_consumption + battery_consumption
 
-    return total_additional_consumption
-
-def main():
-    st.title("Calculadora de consumo de coche eléctrico")
-
-    # Entrada de datos del usuario
-    cx = st.number_input('Introduce el coeficiente aerodinámico (Cx):', min_value=0.0, max_value=1.0, step=0.01)
-    frontal_area = st.number_input('Introduce la superficie frontal del vehículo (m²):')
-    temperature = st.number_input('Introduce la temperatura exterior (°C):')
-    speed = st.number_input('Introduce la velocidad a la que calcular el consumo (km/h):')
-    include_climate_control = st.checkbox('Incluir climatización del habitáculo')
-
-    if st.button('Calcular consumo'):
-        # Convertir la velocidad de km/h a m/s
-        speed_m_s = speed * 1000 / 3600
-        # Calcular el consumo debido a la resistencia aerodinámica
-        aerodynamic_consumption = calculate_aerodynamic_drag(cx, frontal_area, speed_m_s)
-        # Calcular el consumo adicional
-        additional_consumption = calculate_additional_consumption(temperature, include_climate_control)
-        # Calcular el consumo total
-        total_consumption = aerodynamic_consumption + additional_consumption
-
-        st.write(f"El consumo total es: {total_consumption:.2f} kWh cada 100 km")
+        # Output
+        st.subheader('Results')
+        st.write(f'Aerodynamic Consumption: {aero_consumption:.2f} kWh/100km')
+        st.write(f'Battery Consumption: {battery_consumption:.2f} kWh/100km')
+        st.write(f'Total Consumption: {total_consumption:.2f} kWh/100km')
 
 if __name__ == "__main__":
-    main()
-
-
+    app()
