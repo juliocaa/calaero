4042caa22aca0c6bc849a270d73e1da9f2681f03
juliocaa <134210748+juliocaa@users.noreply.github.com>
2023-11-26 16:30:39 +0100

    Update aero_resistence_calculator.py

--- aero_resistence_calculator.py
+++ aero_resistence_calculator.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import ipywidgets as widgets
-from IPython.display import display
 import pandas as pd
 from PIL import Image
 
@@ -12,49 +10,62 @@ st.image(image, caption="AUTOINGENIUM", use_column_width=True)
 
 st.markdown("[VIDEO TUTORIAL](https://www.youtube.com/watch?v=gLxunTcgm1I)")
 
-# Define the function to calculate the electric car consumption
-def calculate_consumption(cx, frontal_area, external_temp, speed, climate_control):
-    # Constants
-    air_density = 1.225  # in kg/m^3 at sea level and at 15°C
-    drag_coefficient = 0.5  # typical value for a passenger car
-    
-    # Calculate aerodynamic resistance consumption
-    aerodynamic_consumption = (1/2) * air_density * frontal_area * cx * (speed/3.6)**2 / 1000  # Convert speed to m/s and consumption to kWh/100km
-    
-    # Calculate battery efficiency based on temperature
-    temperature_factor = 1 - 0.3 * (abs(external_temp - 20) / 20)
-    battery_efficiency_consumption = 5.5 * temperature_factor
-    
-    # Adjust consumption for climate control if necessary
-    if climate_control:
-        climate_control_consumption = 2.0  # Assuming an additional 2 kWh/100km for climate control
+import streamlit as st
+
+def calculate_aerodynamic_drag(cx, frontal_area, speed):
+    # Constante para la densidad del aire a nivel del mar y 15°C
+    air_density = 1.225  # en kg/m^3
+    # Calcula la fuerza de arrastre aerodinámico
+    drag_force = 0.5 * air_density * (speed ** 2) * frontal_area * cx
+    # Suponiendo una potencia constante para superar esta fuerza a la velocidad dada
+    # Potencia = Fuerza * Velocidad
+    # Energía = Potencia * Tiempo
+    # Energía en kWh para 100 km
+    energy_consumption = drag_force * speed / 3600  # Convertir W a kW y segundos a horas
+    return energy_consumption
+
+def calculate_additional_consumption(temperature, include_climate_control):
+    optimal_temperature = 20  # Temperatura óptima en grados Celsius
+    base_consumption = 5.5  # Consumo base en kWh cada 100 km
+
+    # Ajustar el consumo base en función de la pérdida de eficiencia
+    if temperature < optimal_temperature:
+        efficiency_loss = (optimal_temperature - temperature) / 20 * 0.3
     else:
-        climate_control_consumption = 0.0
-    
-    # Total consumption
-    total_consumption = aerodynamic_consumption + battery_efficiency_consumption + climate_control_consumption
-    return total_consumption
+        efficiency_loss = (temperature - optimal_temperature) / 20 * 0.3
 
-# Create interactive widgets
-cx_slider = widgets.FloatSlider(min=0.2, max=0.4, step=0.01, value=0.3, description='Coeficiente CX:')
-frontal_area_slider = widgets.FloatSlider(min=1.0, max=3.0, step=0.1, value=2.2, description='Superficie Frontal:')
-temperature_slider = widgets.IntSlider(min=-20, max=40, step=1, value=20, description='Temperatura Exterior:')
-speed_slider = widgets.FloatSlider(min=50, max=200, step=1, value=100, description='Velocidad (km/h):')
-climate_control_checkbox = widgets.Checkbox(value=False, description='Climatización')
+    adjusted_consumption = base_consumption * (1 + efficiency_loss)
 
-# A function to update the output when any widget value changes
-def update_output(*args):
-    consumption = calculate_consumption(cx_slider.value, frontal_area_slider.value, temperature_slider.value, speed_slider.value, climate_control_checkbox.value)
-    print(f"Consumo total cada 100 km: {consumption:.2f} kWh")
+    # Añadir un consumo fijo si se incluye el control climático
+    climate_control_consumption = 2 if include_climate_control else 0
 
-# Bind the function to changes in widget values
-cx_slider.observe(update_output, 'value')
-frontal_area_slider.observe(update_output, 'value')
-temperature_slider.observe(update_output, 'value')
-speed_slider.observe(update_output, 'value')
-climate_control_checkbox.observe(update_output, 'value')
+    total_additional_consumption = adjusted_consumption + climate_control_consumption
 
-# Display the widgets and the initial consumption calculation
-display(cx_slider, frontal_area_slider, temperature_slider, speed_slider, climate_control_checkbox)
-update_output()  # To display the initial calculation
+    return total_additional_consumption
+
+def main():
+    st.title("Calculadora de consumo de coche eléctrico")
+
+    # Entrada de datos del usuario
+    cx = st.number_input('Introduce el coeficiente aerodinámico (Cx):', min_value=0.0, max_value=1.0, step=0.01)
+    frontal_area = st.number_input('Introduce la superficie frontal del vehículo (m²):')
+    temperature = st.number_input('Introduce la temperatura exterior (°C):')
+    speed = st.number_input('Introduce la velocidad a la que calcular el consumo (km/h):')
+    include_climate_control = st.checkbox('Incluir climatización del habitáculo')
+
+    if st.button('Calcular consumo'):
+        # Convertir la velocidad de km/h a m/s
+        speed_m_s = speed * 1000 / 3600
+        # Calcular el consumo debido a la resistencia aerodinámica
+        aerodynamic_consumption = calculate_aerodynamic_drag(cx, frontal_area, speed_m_s)
+        # Calcular el consumo adicional
+        additional_consumption = calculate_additional_consumption(temperature, include_climate_control)
+        # Calcular el consumo total
+        total_consumption = aerodynamic_consumption + additional_consumption
+
+        st.write(f"El consumo total es: {total_consumption:.2f} kWh cada 100 km")
+
+if __name__ == "__main__":
+    main()
+
 
